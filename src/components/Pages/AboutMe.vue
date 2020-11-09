11857a5fb561c5ac0f40e3f19a71b47b8233a2d0
Alex Summer <summerag@pop-os.localdomain>
2020-11-08 23:12:24 -0500

    initial commit

---
+++ src/components/Pages/AboutMe.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>About Me</h1>
+    <p>This page is about myself</p>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+
+import AboutMe from './components/Pages/AboutMe.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/about', component: AboutMe}
+    ]
+});
+
+
+App.use(router);
 
 createApp(App).mount('#app')
